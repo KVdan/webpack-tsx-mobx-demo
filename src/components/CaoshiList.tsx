6b7496f492e8a9db1a25b691bb94a47a2db0a16f
KVdan <dainewu2017@gmail.com>
2021-08-24 15:49:53 +0800

    update .prettier file && all ts files are formatted

--- src/components/CaoshiList.tsx
+++ src/components/CaoshiList.tsx
@@ -16,40 +16,40 @@ function CaoshiList({ store = new CaoshiStore() }) {
     store.deleteCaoshi(id);
   };
   return (
-    <Layout>
-      <Sider
-        breakpoint="lg"
-        collapsedWidth="0"
-        onBreakpoint={(broken) => {
-          console.log(broken);
-        }}
-        onCollapse={(collapsed, type) => {
-          console.log(collapsed, type);
-        }}
-      >
-        <Menu theme="dark" mode="inline" defaultSelectedKeys={["4"]}>
-          <Menu.Item key="1">nav 1</Menu.Item>
-          <Menu.Item key="2">nav 2</Menu.Item>
-          <Menu.Item key="3">nav 3</Menu.Item>
-          <Menu.Item key="4">nav 4</Menu.Item>
-        </Menu>
-      </Sider>
-      <Content style={{ margin: "2rem" }}>
-        <h2>余{store.totalCaoshis}篇</h2>
-        {store.caoshis.map((caoshi: Caoshi) => {
-          return (
-            <div key={caoshi.id}>
-              <h2>{caoshi.title}</h2>
-              <p>{caoshi.paragraphs}</p>
-              <Button type="link" onClick={() => deleteCaoshi(caoshi.id)}>
-                已读
-              </Button>
-            </div>
-          );
-        })}
-      </Content>
-    </Layout>
-  );
+		<Layout>
+			<Sider
+				breakpoint='lg'
+				collapsedWidth='0'
+				onBreakpoint={broken => {
+					console.log(broken);
+				}}
+				onCollapse={(collapsed, type) => {
+					console.log(collapsed, type);
+				}}
+			>
+				<Menu theme='dark' mode='inline' defaultSelectedKeys={['4']}>
+					<Menu.Item key='1'>nav 1</Menu.Item>
+					<Menu.Item key='2'>nav 2</Menu.Item>
+					<Menu.Item key='3'>nav 3</Menu.Item>
+					<Menu.Item key='4'>nav 4</Menu.Item>
+				</Menu>
+			</Sider>
+			<Content style={{margin: '2rem'}}>
+				<h2>余{store.totalCaoshis}篇</h2>
+				{store.caoshis.map((caoshi: Caoshi) => {
+					return (
+						<div key={caoshi.id}>
+							<h2>{caoshi.title}</h2>
+							<p>{caoshi.paragraphs}</p>
+							<Button type='link' onClick={() => deleteCaoshi(caoshi.id)}>
+								已读
+							</Button>
+						</div>
+					);
+				})}
+			</Content>
+		</Layout>
+	);
 }
 
 export default observer(CaoshiList);
